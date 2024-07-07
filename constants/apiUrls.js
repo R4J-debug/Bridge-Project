c7bd71ae25bac0d547e348b58becf980744c6041
R4J-debug <85886888+R4J-debug@users.noreply.github.com>
2024-07-07 14:53:45 +0530

    Delete constants directory

--- constants/apiUrls.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    recommendedTokens: 'https://aggregator-api.xy.finance/v1/recommendedTokens',
-    quote: 'https://aggregator-api.xy.finance/v1/quote',
-    buildTx: 'https://aggregator-api.xy.finance/v1/buildTx'
-};
